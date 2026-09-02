da3c11e03951ab542709c310a9dc4f91c30c73af
Nounu90 <115592907+Nounu90@users.noreply.github.com>
2026-09-02 12:28:31 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* MSTORECASA - service worker */
-const V = "mstorecasa-v27";
+const V = "mstorecasa-v29";
 const CORE = [
   "./", "./index.html", "./manifest.json", "./supabase.js",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png", "./apple-touch-icon.png"
